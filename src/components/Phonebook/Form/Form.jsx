96683c1f07f9c77b02a2676ab7838026377c0bd9
MakrFrost <frostmark@ukr.net>
2022-10-26 23:18:17 +0300

    MK III

---
+++ src/components/Phonebook/Form/Form.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  inputChange = evt => {
+    const { name, value } = evt.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  clearForm = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  onFormSubmit = evt => {
+    evt.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.clearForm();
+  };
+
+  render() {
+    // const randomId = nanoid();
+
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <label>
+            Name:
+            <input
+              value={this.state.name}
+              onChange={this.inputChange}
+              name="name"
+              type="text"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number:
+            <input
+              value={this.state.number}
+              onChange={this.inputChange}
+              name="number"
+              type="tel"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+
+          <button type="submit">Add contact!</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+// import { nanoid } from 'nanoid';
+
+import Form from './Form/Form';
+
+class Phonebook extends Component {
+  onFormSubmitData = data => {
+    console.log(data);
+  };
+
+  render() {
+    // const randomId = nanoid();
+
+    return (
+      <div>
+        <Form onSubmit={this.onFormSubmitData} />
+        <h1>Contacts:</h1>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Phonebook;
